cf679a8b20856a4de38a676260cff3a3f1c63d8e
dev_1 <info@qualitythought.in>
2017-04-06 07:21:01 +0530

    Added explicit restart

--- Modules/apache/manifests/homepage.pp
+++ Modules/apache/manifests/homepage.pp
@@ -3,10 +3,20 @@
 #
 class apache::homepage inherits apache::params{
 
-    file { '/var/www/index.html':
+    file { '/var/www/html/index.html':
         ensure => file,
        # source => 'puppet:///modules/index.html';
        content => file('apache/index.html')
+    }
+
+    service { "my service restart":
+        name       => $apache::params::pack_name
+        ensure     => running,
+        enable     => true,
+        hasrestart => true,
+        hasstatus  => true,
+        restart    => "",
+        # pattern    => 'my service restart',
     }
 
      #$pack_name = $facts['os']['family'] ? {
@@ -15,13 +25,5 @@ class apache::homepage inherits apache::params{
    #}
 
 
-    service { $apache::params::pack_name:
-        ensure     => running,
-        enable     => true,
-        hasrestart => true,
-        hasstatus  => true,
-        restart    => ,
-        # pattern    => 'test',
-    }
-     
+         
 }
