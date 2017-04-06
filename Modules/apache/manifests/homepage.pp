e23723a8ed30ccc7f9044e3949d2042f73f4f102
dev_1 <info@qualitythought.in>
2017-04-06 07:57:36 +0530

    Added Notify

--- Modules/apache/manifests/homepage.pp
+++ Modules/apache/manifests/homepage.pp
@@ -6,18 +6,11 @@ class apache::homepage inherits apache::params{
     file { '/var/www/html/index.html':
         ensure => file,
        # source => 'puppet:///modules/index.html';
-       content => file('apache/index.html')
+       content => file('apache/index.html'),
+       notify =>  Service["${apache::params::pack_name}"]
     }
 
-    service { "my service restart":
-        name       => $apache::params::pack_name
-        ensure     => running,
-        enable     => true,
-        hasrestart => true,
-        hasstatus  => true,
-        restart    => "",
-        # pattern    => 'my service restart',
-    }
+    #Resource Relationships
 
      #$pack_name = $facts['os']['family'] ? {
      #  'RedHat' => 'httpd',
